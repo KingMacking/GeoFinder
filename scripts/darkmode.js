e992acfc1cc6f7e9de755a9929311459b2a4b8b9
King Macking <matiasatzori@gmail.com>
2022-08-13 14:18:11 -0300

    Agregado icono, correccion del logo en darkmode, darkmode se aplica a todas las paginas gracias al storage

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,7 +1,19 @@
-function changeMode(){
-    const element = document.body;
-    element.classList.toggle("dark-mode");
-}
 
 const switchDarkMode = document.querySelector("#flexSwitchCheckDefault");
-switchDarkMode.addEventListener("click", changeMode);
+const body = document.body;
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark"){
+    body.classList.add("dark-mode");
+}
+
+switchDarkMode.addEventListener("click", function(){
+    body.classList.toggle("dark-mode");
+
+    let theme = "light";
+    if (body.classList.contains("dark-mode")){
+        theme = "dark";
+    }
+
+    localStorage.setItem("theme", theme);
+})
