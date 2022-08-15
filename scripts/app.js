ec2f232b8daec964a24f193348b00863bfe7df26
King Macking <matiasatzori@gmail.com>
2022-08-15 16:36:07 -0300

    Agregado sistema de estadisticas

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,25 @@
-import {countryData} from './countries.js'
+import {countryData} from './countries.js';
+import {getStatsData} from './stats.js';
 
 //-----Variables-----
 const guess = document.querySelectorAll(".guess");
 const guessButton = document.querySelector("#btnGuess");
 const countrySelect = document.querySelector("#countrySelect");
 const guesses = document.querySelector("#guesses");
-const flag = document.querySelector("#flag-image")
+const flag = document.querySelector("#flag-image");
+const statsButtonOpen = document.querySelector("#stats-button");
+const statsModalBody = document.querySelector("#stats-modal-body")
+const statsButtonClose = document.querySelector("#close-stats-modal")
 
 let guessNumber = 0
 let countryA = countryData[Math.round(Math.random()*countryData.length)];
+let storedStats = JSON.parse(localStorage.getItem("stats"));
 console.log(countryA);
 
 //-----Events-----
 guessButton.addEventListener("click", compareCountries);
+statsButtonOpen.addEventListener("click", showStatsModal);
+statsButtonClose.addEventListener("click", clearModalStats);
 
 //-----Functions-----
 
@@ -51,18 +58,22 @@ function compareCountries(){
             guess[guessNumber].innerText="Debes elegir un pais";
         }
     } else {
-        guess[guessNumber].innerText = "¡Muy bien! El pais a adivinar era " + countryA.name
-        guess[guessNumber].classList.add("win", "fw-bold");
-        guessButton.disabled = true;
-        flag.src = `https://flagcdn.com/${countryA.code}.svg`
+        sendAlert("win");
+        const tryCount = guessNumber + 1;
+        userStats.guessDistribution[tryCount]++;
+        userStats.currentStreak++;
+        userStats.playedCount++;
+        userStats.winCount++;
+        // saveLocal("stats", JSON.stringify(userStats));
+        updateStats(userStats);
+        // localStorage.removeItem("stats");
     }
     if (guessNumber==6){
-        guessButton.disabled = true;
-        let loseAlert = document.createElement("div");
-        loseAlert.innerText = `¡Has perdido! El pais a adivinar era ${countryA.name.toUpperCase()}`
-        loseAlert.classList.add("guess", "p-3", "text-center", "lose", "mt-4", "fw-bold", "h-auto");
-        guesses.appendChild(loseAlert);
-        flag.src = `https://flagcdn.com/${countryA.code}.svg`
+        sendAlert("lose");
+        userStats.currentStreak = 0;
+        userStats.playedCount++;
+        // saveLocal("stats", JSON.stringify(userStats));
+        updateStats(userStats);
     }
 }
 
@@ -116,7 +127,86 @@ function getDistance (lat1, lon1, lat2, lon2) {
     return d;
 }
 
-//Replace incognito flag with country flag
-function replaceFlag (){
+//Win & Lose alerts
+function sendAlert(alertType){
+    if (alertType == "win"){
+        guess[guessNumber].innerText = "¡Muy bien! El pais a adivinar era " + countryA.name;
+        guess[guessNumber].classList.add("win", "fw-bold");
+    } else if (alertType == "lose"){
+        let loseAlert = document.createElement("div");
+        loseAlert.innerText = `¡Has perdido! El pais a adivinar era ${countryA.name.toUpperCase()}`;
+        loseAlert.classList.add("guess", "p-3", "text-center", "lose", "mt-4", "fw-bold", "h-auto");
+        guesses.appendChild(loseAlert);
+    }
+    flag.src = `https://flagcdn.com/${countryA.code}.svg`;
+    guessButton.disabled = true;
+}
 
+const saveLocal = (key, value) => {localStorage.setItem(key, value)};
+let userStats = {
+    guessDistribution: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+    },
+    currentStreak : 0,
+    maxStreak : 0,
+    winCount : 0,
+    playedCount : 0,
+}
+
+//-----Stats-----
+//Update stats
+function updateStats(updatedStats){
+    console.log(storedStats);
+    if (storedStats != null){
+        if(updatedStats.currentStreak != 0){
+            storedStats.currentStreak += updatedStats.currentStreak;
+        } else {
+            storedStats.currentStreak = 0;
+        }
+        storedStats.winCount += updatedStats.winCount;
+        storedStats.playedCount += updatedStats.playedCount;
+        for (let guessTry in storedStats.guessDistribution){
+            storedStats.guessDistribution[guessTry] += updatedStats.guessDistribution[guessTry];
+        }
+        if (storedStats.currentStreak >= storedStats.maxStreak){
+            storedStats.maxStreak = storedStats.currentStreak;
+        }
+        localStorage.setItem("stats", JSON.stringify(storedStats));
+    }else {
+        localStorage.setItem("stats", JSON.stringify(userStats));
+    }
+}
+
+//Get stats to show on modal
+function showStatsModal(){
+    let guessStats = document.createElement("div");
+    let guessDistributionStats = document.createElement("div");
+    let winPercentage = Math.round((storedStats.winCount/storedStats.playedCount)*100)
+    guessStats.innerHTML = `<div class="text-center fs-3"><p class="fw-bolder">${storedStats.currentStreak}</p><p>Racha</p></div>
+                            <div class="text-center fs-3"><p class="fw-bolder">${storedStats.maxStreak}</p><p>Mejor racha</p></div>
+                            <div class="text-center fs-3"><p class="fw-bolder">${storedStats.winCount}</p><p></p>Ganadas</div>
+                            <div class="text-center fs-3"><p class="fw-bolder">${storedStats.playedCount}</p><p>Jugadas</p></div>
+                            <div class="text-center fs-3"><p class="fw-bolder">${winPercentage}</p><p>% Ganadas</p></div>`;
+    guessStats.classList.add("d-flex", "justify-content-between");
+    guessDistributionStats.innerHTML = `<h2 class="text-center">DISTRIBUCION DE ACIERTOS</h2>
+                                        <p><span>1: </span><span>${storedStats.guessDistribution[1]}</span></p>
+                                        <p><span>2: </span><span>${storedStats.guessDistribution[2]}</span></p>
+                                        <p><span>3: </span><span>${storedStats.guessDistribution[3]}</span></p>
+                                        <p><span>4: </span><span>${storedStats.guessDistribution[4]}</span></p>
+                                        <p><span>5: </span><span>${storedStats.guessDistribution[5]}</span></p>
+                                        <p><span>6: </span><span>${storedStats.guessDistribution[6]}</span></p>`
+    guessDistributionStats.classList.add("fs-3", "border-top", "py-3")
+    statsModalBody.classList.add("p-5")
+    statsModalBody.appendChild(guessStats)
+    statsModalBody.appendChild(guessDistributionStats)
+}
+
+//Clear stats in modal to update them
+function clearModalStats(){
+    statsModalBody.innerHTML = ``
 }
